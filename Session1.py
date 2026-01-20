6d798d887b73db44e5a2a3f191212f9c0128335d
stokesshaun126 <stokesshaun126@gmail.com>
2026-01-20 16:59:09 +0100

    My first commit ever

---
+++ Session1.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("7+6")
+print(7+6)
+print("6 + 7 =", 6+7)
+print("I have $", 100+200, ".", sep="!!")
+# end is used to tell print what to do after it prints
+print("1", end="")
+print("2", end="")
+print("3", end="hello")
+print("4", end="\n\n")
+print("5", end="")
+# \n adds a space between the lines that are being printed
